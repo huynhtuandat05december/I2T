c0805ce66836320787d4b22777b220fae39008e2
huynhtuandat05december <huynhtuandat05december@gmail.com>
2024-03-30 14:39:16 +0700

    first commit

---
+++ setup.sh
@@ -0,0 +1,11 @@
+pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu121/torch2.1/index.html
+
+
+git clone https://github.com/facebookresearch/detectron2.git
+cd detectron2
+git checkout cc87e7ec
+pip install -e .
+ 
+cd /content/I2T
+pip install -r requirements.txt
+pip install -r requirements_GRiT.txt
